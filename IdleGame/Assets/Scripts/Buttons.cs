556cce1b839932d0a7393d496f5fe58b17f79cde
Bulbakin <julius.kulju@hotmail.fi>
2018-04-10 21:41:58 +0300

    Romi syö

---
+++ IdleGame/Assets/Scripts/Buttons.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void onLahnaPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onSushiPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onMuikkuPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onPerunaPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onRaejuustoPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onNautaPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onRapepalatPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+    public void onKanaPress()
+    {
+        anim.SetTrigger("RomiEats");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
