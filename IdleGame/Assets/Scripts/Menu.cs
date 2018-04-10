a72cdc34540fc80c72381bb31f011ca27dea6440
Bulbakin <julius.kulju@hotmail.fi>
2018-04-10 17:47:48 +0300

    Pommitin sun himan

---
+++ IdleGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void openCloseMenu(Animator anim)
+    {
+        if (anim.GetBool("OpenMenu") == false)
+        {
+            anim.SetBool("OpenMenu", true);
+
+        } else
+        {
+            anim.SetBool("OpenMenu", false);
+        }
+    }
+}
